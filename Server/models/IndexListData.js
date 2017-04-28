6bf4001cc81a131236786814381d48b39dfdc6af
375670450 <375670450@qq.com>
2017-04-28 21:09:23 +0800

    04-28 21:09 finish combination

--- Server/models/IndexListData.js
+++ Server/models/IndexListData.js
@@ -1,25 +1,13 @@
-var mysql = require('mysql')
 
-var config = {
-    host: "112.74.124.145",
-    user: "group5",
-    password: "group5",
-    port: "3306",
-    database: "stockG5"
-};
+var getConnection = require('./DbConnection')
+
 
 var pool = mysql.createPool(config)
 
 class IndexListData{
-    constructor() {
-    }
-
-    static getConnection(){
-        return pool
-    }
 
     static getIndexList(callback) {
-        var conn = IndexListData.getConnection()
+        var conn = getConnection()
         conn.query("SELECT * FROM indexList", callback)
     }
 }
